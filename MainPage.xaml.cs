1c6a54aba03444036aff582e7b0202a8d85ba5fc
FMaged <fadelmaged25@gmail.com>
2024-12-24 15:14:41 +0100

    Added the Main Page

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,20 @@
             InitializeComponent();
         }
 
+        private void CameraFrameTapped(object sender, EventArgs e) 
+        {
+            DisplayAlert("Tapped!", "You tapped the Camera frame.", "OK");
+        }
+
+        private void GallareyFrameTapped(Object sender, EventArgs e)
+        {
+            DisplayAlert("Tapped!", "You tapped the Gallarey frame.", "OK");
+        }
+        private void Enter_BarcodeFrameTapped(Object sender, EventArgs e)
+        {
+            DisplayAlert("Tapped!", "You tapped the Enter_Barcode frame.", "OK");
+        }
+
         private void BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
         {
             var result= e.Results?.FirstOrDefault()?.Value;
