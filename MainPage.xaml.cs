9850c9c848d83c6936e49496fa08faf2dbcd47d3
FMaged <fadelmaged25@gmail.com>
2024-12-24 14:40:51 +0100

    Projektdateien hinzufügen.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Scanner
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
+        {
+            var result= e.Results?.FirstOrDefault()?.Value;
+            if (result != null)
+            {
+                Console.WriteLine($"Detected barcode: {result}");
+            }
+        }
+    }
+
+}
